edaa3494b8541d1038b6351b2f9066001f20e63a
Mitchell <soggybag@gmail.com>
2021-02-25 10:50:05 -0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container } from 'reactstrap'
+
+import PostViewer from './PostViewer'
+import PostEditor from './PostEditor'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      editing: null
+    }
+  }
+
   render() {
+    const { editing } = this.state
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Container fluid>
+        <button
+          className="my-2"
+          color="primary"
+          onClick={() => this.setState({ editing: {} })}
+        >
+          New Post
+        </button>
+        <PostViewer
+          canEdit={() => true}
+          onEdit={(post) => this.setState({ editing: post })}
+        />
+        {editing && (
+          <PostEditor 
+            post={editing}
+            onClose={() => this.setState({ editing: null })}
+          />
+        )}
+      </Container>
     );
   }
 }
